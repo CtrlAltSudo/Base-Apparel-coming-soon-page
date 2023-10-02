a919b57126e6e505bda4221812b65de7d7e57275
Damien White <damienkwhite@gmail.com>
2023-10-02 21:04:04 +0100

    added form error icon

--- app.js
+++ app.js
@@ -2,13 +2,15 @@ const form = document.querySelector("#email");
 
 
 form.addEventListener("input", inputErr);
-
 function inputErr() {
     if(form.value.includes("@") && form.value.includes("")) {
         console.log("it's a valid email address")
+        document.getElementById("formErrMsg").style.display = "none"; 
+
     } else {
         console.log("it's not valid")
         form.classList.add("formErr")
+        document.getElementById("formErrMsg").style.display = "block"; 
         
     }
 }
