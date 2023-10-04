5403ab7f77715997994a160fa597bc2280805375
Damien White <damienkwhite@gmail.com>
2023-10-04 20:29:19 +0100

    completed challenge

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
 const form = document.querySelector("#email");
-
+const input = document.querySelector("input");
 
 form.addEventListener("input", inputErr);
 function inputErr() {
     if(form.value.includes("@") && form.value.includes("")) {
         console.log("it's a valid email address")
         document.getElementById("formErrMsg").style.display = "none"; 
+        input.style.backgroundImage = "none"
 
     } else {
         console.log("it's not valid")
         form.classList.add("formErr")
         document.getElementById("formErrMsg").style.display = "block"; 
+        input.style.backgroundImage = "url(images/icon-error.svg)"
         
     }
 }
